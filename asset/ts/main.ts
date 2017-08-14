f0b9a352432dbb6287b8d42eed76140ca0de3ae5
Kousher alam <kousheralampranto@gmail.com>
2017-08-14 16:14:45 +0600

    add all asset files

---
+++ asset/ts/main.ts
@@ -0,0 +1,7 @@
+import {Something} from './other/mod1';
+
+let myName:string = `kousher alam`;
+console.log(`hello ${myName}`);
+
+console.log((new Something).name);
+console.log(`hello there again..`);
