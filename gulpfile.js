6369d1f9b2c60309e5ae17161dc30b2869f5cbff
Kousher alam <kousheralampranto@gmail.com>
2017-08-14 15:58:28 +0600

    Add Sass && ts && imagemin support
    Sass -- autoprefix, sourcemap completer
    imagemin
    ts ---  compaile , typings, td.ts. and map

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,46 @@
-var gulp = require('gulp');  
-var sass = require('gulp-sass');  
-var browserSync = require('browser-sync');
-var ts = require("gulp-typescript");
-var tsProject = ts.createProject("tsconfig.json");
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    imagemin = require('gulp-imagemin'),
+    ts = require("gulp-typescript");
+    // tsProject = ts.createProject("tsconfig.json");
 
 gulp.task('sass', function () {  
     gulp.src('asset/sass/main.sass')
-        .pipe(sass({includePaths: ['asset/sass']}))
+        .pipe(sourcemaps.init())
+            .pipe(sass({includePaths: ['asset/sass']}))
+            .pipe(autoprefixer({
+                browsers: ['last 20 versions','>1%'],
+                cascade: false
+            }))
+        .pipe(sourcemaps.write('map'))        
         .pipe(gulp.dest('dist/css'));
 });
 
+
+//Complile ts with source map... 
 gulp.task('ts', function () {  
-    return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest("dist"));
+    return gulp.src('asset/ts/**/*.ts')
+        .pipe(sourcemaps.init())
+            .pipe(ts({
+                target: 'ES5',
+                module: 'system',
+                noImplicitAny: true,
+                declaration: true,
+                emitDecoratorMetadata: true,
+                experimentalDecorators: true,
+                typeRoots: [
+                    "asset/@types"
+                ]
+            }))
+        .pipe(sourcemaps.write('map')) 
+        .pipe(gulp.dest('dist/js'));
 });
 
+
+//browser sync
 gulp.task('browser-sync', function() {  
     browserSync.init(["asset/**"], {
         server: {
@@ -24,6 +49,27 @@ gulp.task('browser-sync', function() {
     });
 });
 
+
+//gulp task for image Comporession
+gulp.task('image', function(){
+    gulp.src('asset/img/**')
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.jpegtran({progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({plugins: [{removeViewBox: true}]})
+        ]))
+        .pipe(gulp.dest('dist/img'));
+});
+
+
+//gulp task for production version
+gulp.task('dist',function(){
+
+});
+
+
 gulp.task('default', ['sass', 'browser-sync','ts'], function () {  
-    gulp.watch("asset/**", ['sass']);
+    gulp.watch("asset/sass/**", ['sass']);
+    gulp.watch("asset/ts/**", ['ts']);
 });
